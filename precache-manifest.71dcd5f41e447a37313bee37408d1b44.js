d68929110221f6491c3930c643a19f65e8caf614
이준엽 사원 NCD개발팀 (wnsduq7133@lgcns.com <82170@CORP.LGCNS.COM>
2020-04-30 14:12:07 +0900

    Updates

---
+++ precache-manifest.71dcd5f41e447a37313bee37408d1b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6e03ffbd8e4ab2bef49384cca950b6",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4161118288698b2b38d5",
+    "url": "/movie_app_2020/static/css/main.52e68f08.chunk.css"
+  },
+  {
+    "revision": "3a4995f101c8bb4fa292",
+    "url": "/movie_app_2020/static/js/2.6767d2ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.6767d2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4161118288698b2b38d5",
+    "url": "/movie_app_2020/static/js/main.425cbbea.chunk.js"
+  },
+  {
+    "revision": "e6ea8d87ddc55749b444",
+    "url": "/movie_app_2020/static/js/runtime-main.4394d953.js"
+  }
+]);
